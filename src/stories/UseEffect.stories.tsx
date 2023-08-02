325879df984d29a6cd22b179017d147c5003feba
EvgeniyaYakunina <Yakoonina@gmail.com>
2023-08-02 11:39:50 +0300

    add UseEffect(setTimeout, setInterval)

--- src/stories/UseEffect.stories.tsx
+++ src/stories/UseEffect.stories.tsx
@@ -3,6 +3,7 @@ import {useEffect, useState} from "react";
 export default {
     title: 'useEffect demo'
 }
+
 export const SimpleExample = ()=>{
     const [fake, setFake]= useState(1)
     const [counter, setCounter]= useState(1)
@@ -39,4 +40,41 @@ export const SimpleExample = ()=>{
     </>
 }
 
+export const SetTimeoutExample = ()=>{
+    const [fake, setFake]= useState(1)
+    const [counter, setCounter]= useState(1)
 
+    console.log("SetTimeoutExample");
+
+    useEffect(()=> {
+        setTimeout(()=>{
+            console.log("setTimeout")
+            document.title = counter.toString();
+        }, 1000)
+    },[counter])
+
+    return <>
+        Hello,{counter} {fake}
+        <button onClick={()=>setCounter(counter+1)}>counter+</button>
+        <button onClick={()=>setFake(fake+1)}>fake+</button>
+    </>
+}
+
+export const SetIntervalExample = ()=>{
+    const [fake, setFake]= useState(1)
+    const [counter, setCounter]= useState(1)
+
+    console.log("SetIntervalExample");
+
+    useEffect(()=> {
+        setInterval(()=>{
+            setCounter((state)=> state + 1);
+        }, 1000)
+    },[])
+
+    return <>
+        Hello,counter: {counter} - fake: {fake}
+        {/*<button onClick={()=>setCounter(counter+1)}>counter+</button>*/}
+        {/*<button onClick={()=>setFake(fake+1)}>fake+</button>*/}
+    </>
+}
